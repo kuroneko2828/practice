b195f2a780f901f122b6566a76a0a1a804cf7407
kuroneko2828 <nekonekonya.kuro@gmail.com>
2021-12-02 17:01:46 +0900

    Add flask

---
+++ flask/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    name = "Hello, World!"
+    return render_template("hello.html", title='flask test', name=name)
+
+@app.route('/good')
+def good():
+    name = "Good!"
+    return name
+
+if __name__ == "__main__":
+    app.run(debug=True)
