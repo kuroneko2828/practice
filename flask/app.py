950cc5ea54af640b6f7572a0ecb9c9aed459f9be
kuroneko2828 <nekonekonya.kuro@gmail.com>
2021-12-09 19:42:54 +0900

    add bootstrap test

--- flask/app.py
+++ flask/app.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import psycopg2
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
 
 @app.route('/hello/<name>')
 def hello(name=None):
@@ -23,6 +27,24 @@ def hello(name=None):
 def good():
     name = "Good!"
     return name
+
+@app.route('/add')
+def add():
+    return render_template("add.html")
+
+@app.route('/add_manager', methods=["POST"])
+def add_manager():
+    if request.method=="POST":
+        name = request.form["name"]
+    else:
+        name = "no name."
+    return "ようこそ、"+name+"さん。"
+
+@app.route('/get_test')
+def get_test():
+    name = request.args.get('name')
+    return "ようこそ、"+name
+
 
 if __name__ == "__main__":
     app.run(debug=True)
