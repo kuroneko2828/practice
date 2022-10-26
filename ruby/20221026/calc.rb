89643e28d83916b7f755c7e1f518fe760ebc04ee
kuroneko2828 <nekonekonya.kuro@gmail.com>
2022-10-26 19:13:51 +0900

    add ruby calc

---
+++ ruby/20221026/calc.rb
@@ -0,0 +1,27 @@
+n1 = 10
+n2 = 3.14
+puts(n1)
+puts(n1.class)
+puts(n2)
+puts(n2.class)
+n3 = 0xab
+puts(n3)
+puts("-----")
+n = 123_456_789
+puts(n)
+puts("-----")
+n1 = 3
+n2 = 4
+n3 = 0.5
+puts(n1**n2)
+puts(n1**n3)
+puts("-----")
+n1 = 6
+n2 = 4
+puts(n1/n2)
+puts(Float(n1))
+puts(Float(n1)/n2)
+puts("-----")
+n1 = 0b1101
+n2 = 0b1110
+puts(n1 & n2)
