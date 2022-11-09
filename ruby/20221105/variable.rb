e44d245e38535315055333a0c98d316652e7e1a0
kuroneko2828 <nekonekonya.kuro@gmail.com>
2022-11-09 14:58:07 +0900

    variable

---
+++ ruby/20221105/variable.rb
@@ -0,0 +1,16 @@
+msg = "Hello"
+msg2 = msg
+msg2 << " World"
+puts(msg)
+msg3 = msg2
+msg3 += "!"
+puts(msg2)
+puts(msg3)
+
+puts("-----")
+
+v1, v2, v3, v4 = "東京都", "大阪府", "福岡県"
+puts(v1)
+puts(v2)
+puts(v3)
+puts(v4)
